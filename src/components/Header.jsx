25064e4694839148329768f227301c64f0f1ee27
tinaytini <tinaytini@yahoo.com>
2022-04-18 12:49:18 +0200

    created header-top content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import "./Header.scss";
+
 function Header() {
     return (
-        <header>
-            <h1>hello</h1>
+        <header className="header">
+            <div className="shop-info">
+                <div className="container shop-info__content">
+                    <div className="open__time">
+                        <span>Mon-Thu: </span>  
+                        9:00 AM - 5:30 PM</div>
+                    <div className="header__address">
+                        <span>
+                            Visit our showroom in 1234 Street Adress City Address, 1234
+                        </span>  
+                        <a className="address__link"> Contact Us</a>
+                    </div>
+                    <div className="header__contact">
+                        <a href="tel:+(00) 1234 5678">Call Us: (00) 1234 5678</a>
+                        <a href="https://www.facebook.com/" className="header__facebook">
+                            <img src="/assets/images/socials/facebook.svg" alt="facebook"/>
+                        </a>
+                        <a href="https://www.instagram.com/" className="header__instagram">
+                            <img src="/assets/images/socials/instagram.svg" alt="facebook"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
